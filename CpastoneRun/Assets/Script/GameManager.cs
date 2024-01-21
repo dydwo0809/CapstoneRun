99b30eeb9e9d1f31b2abeef78303c1ceda15325b
dydwo0809 <dydwo0809@naver.com>
2024-01-21 23:13:21 +0900

    GameManager 추가
    GameManager 스크립트 싱글톤 패턴으로 작성

---
+++ CpastoneRun/Assets/Script/GameManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                if (!_instance)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = "GameManager";
+                    _instance = obj.AddComponent(typeof(GameManager)) as GameManager;
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public float score { get; set; } = 0;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void addScore(float added)
+    {
+        score += added;
+    }
+}
+
---
+++ CpastoneRun/Assets/Script/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameManager.Instance.addScore(0.1f);
+        float score = GameManager.Instance.score;
+        Debug.Log(score);
+    }
+}
