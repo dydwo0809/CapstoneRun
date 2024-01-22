0405b0cfe5f25034fe9b6eddd925e4f17159b18f
Noewewwwew <dnjs4475@naver.com>
2024-01-23 04:27:12 +0900

    움직임, 애니메이션 구현 중
    미완

--- CpastoneRun/Assets/Script/PlayerControl.cs
+++ CpastoneRun/Assets/Script/PlayerControl.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float runSpeed;
     public float jumpPower;
     bool isJump;
     bool isDoubleJump;
+    private Vector3 velocity;
 
     public AudioClip audioRun;
     public AudioClip audioJump;
@@ -14,16 +16,21 @@ public class PlayerControl : MonoBehaviour
     public AudioClip audioDie;
     public AudioClip audioFinish;
     public AudioClip audioLanding;
-
+    
     private Rigidbody myRigid;
     AudioSource audioSource;
-
+    private Animator anim;
+    private CapsuleCollider col;
+    static int locoState = Animator.StringToHash ("Base Layer.Locomotion");
+    private AnimatorStateInfo currentBaseState;
 
     void Start()
     {
         isJump = false;
         myRigid = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator> ();
+        col = GetComponent<CapsuleCollider> ();
     }
 
     void Update()
@@ -46,15 +53,35 @@ public class PlayerControl : MonoBehaviour
         // Control
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        anim.SetFloat ("Speed", v);						
+		anim.SetFloat ("Direction", h);
         
-        myRigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+        velocity = new Vector3 (h, 0, v);		
+			
+		velocity = transform.TransformDirection (velocity);
+        if (v > 0.1) {
+            velocity *= runSpeed;
+        } else if (v < -0.1) {
+            velocity *= runSpeed;
+        }
+
+        if (Input.GetButtonDown ("Jump")) {            
+            if (currentBaseState.nameHash == locoState) {
+                if (!anim.IsInTransition (0)) {
+                    myRigid.AddForce (Vector3.up * jumpPower, ForceMode.VelocityChange);
+                    anim.SetBool ("Jump", true);
+                }
+            }
+        }
     }
 
     void OnCollisionEnter(Collision collision){
         // 땅에 닿아 있는지(여기선 Plane)
         if(collision.gameObject.name == "Plane"){
+            Debug.Log("Coasclasclasc");
             isJump = false;
             isDoubleJump = false;
+            PlaySound("LANDING");
         }
     }
 
