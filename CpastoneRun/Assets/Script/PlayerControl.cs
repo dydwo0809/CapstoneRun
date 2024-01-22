4662044a3e9a512e4806f55e6fe1255b99edf747
Noewewwwew <dnjs4475@naver.com>
2024-01-23 02:55:02 +0900

    플레이어 조작 및 사운드 틀 제작
    전후좌우 이동, 점프 구현
    사운드 틀 제작(사운드 넣기만 하면 됨)

---
+++ CpastoneRun/Assets/Script/PlayerControl.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float jumpPower;
+    bool isJump;
+    public AudioClip audioRun;
+    public AudioClip audioJump;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
+    public AudioClip audioLanding;
+
+    private Rigidbody myRigid;
+    AudioSource audioSource;
+
+
+    void Start()
+    {
+        isJump = false;
+        myRigid = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Jump
+        if(Input.GetButtonDown("Jump") && !isJump){
+            isJump = true;
+            myRigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+            PlaySound("JUMP");
+        }
+    }
+
+    void FixedUpdate(){
+        // Control
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        
+        myRigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision){
+        // 땅에 닿아 있는지(여기선 Plane)
+        if(collision.gameObject.name == "Plane")
+            isJump = false;
+    }
+
+    void PlaySound(string action){
+        switch(action){
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "RUN":
+                audioSource.clip = audioRun;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+            case "LANDING":
+                audioSource.clip = audioLanding;
+                break;
+        }
+    }
+}
