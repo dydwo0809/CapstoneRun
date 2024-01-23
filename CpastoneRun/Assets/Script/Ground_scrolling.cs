0764d62f7d5b7c49964b94600d7aee4e27dfaf42
likeIt0210 <wlwlfdl3324@kookmin.ac.kr>
2024-01-23 23:52:52 +0900

    점수에 따라 속도가 빨라지도록 변경
    Ground_scrolling에 speed_raiseRate변수를 선언. 점수의 speed_raiseRate 만큼 속도를 증가시킴. public으로 선언하여 인스펙터 창에서도 변경 가능하도록 함. 근데 아직 점수가 오르는건 구현이 안돼서 잘 실행되는지 확인은 못했음

--- CpastoneRun/Assets/Script/Ground_scrolling.cs
+++ CpastoneRun/Assets/Script/Ground_scrolling.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ground_scrolling : MonoBehaviour
 {
     public float speed = 20f;
+    public float speed_raiseRate = 0.1f;
+
+    float score = GameManager.Instance.score;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +17,8 @@ public class Ground_scrolling : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        speed += speed_raiseRate * score;
         transform.Translate(Vector3.back* speed * Time.deltaTime);
+
     }
 }
