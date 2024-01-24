a76353bed8b702817cf0bf8608e8593cdc5ee262
shware10 <112537781+shware10@users.noreply.github.com>
2024-01-24 18:23:05 +0900

    아이템 움직임 추가
    아이템 움직임 추가

--- CpastoneRun/Assets/Script/Item.cs
+++ CpastoneRun/Assets/Script/Item.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 pos;
+    private float max = 0.3f;
+    private float speed = 2f;
+    private float x = 0;
     void Start()
     {
-        
+        transform.rotation = Quaternion.identity;
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = pos;
+        v.y += max * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+        x += Time.deltaTime * 50;
+        transform.rotation = Quaternion.Euler(0, x, 0);
     }
 
     // Update is called once per frame
