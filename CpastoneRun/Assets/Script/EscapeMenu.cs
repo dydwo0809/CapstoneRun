7057a5dec3ee40efa8c9c552846036cb853c1611
shware10 <112537781+shware10@users.noreply.github.com>
2024-01-24 16:49:26 +0900

    esc 정지 추가 / 새 게임/ 메인메뉴 버튼 추가
    esc 정지 추가 / 정지상태에서 새 게임/ 메인메뉴 버튼 추가

---
+++ CpastoneRun/Assets/Script/EscapeMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EscapeMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    public GameObject escapeMenu;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(IsPaused == false)
+            {
+                escapeMenu.SetActive(true);
+                Time.timeScale = 0f;
+                IsPaused = true;
+            }
+            else
+            {
+                escapeMenu.SetActive(false);
+                Time.timeScale = 1f;
+                IsPaused = false;
+            }
+        }
+    }
+
+    public void OnClickedNewGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OnClickedMainMenu()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
