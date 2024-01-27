8094613b686b253551602ef9e549f50f3647f135
dydwo0809 <dydwo0809@naver.com>
2024-01-27 18:17:26 +0900

    아이템 변경
    더하기, 빼기 아이템 추가

---
+++ CpastoneRun/Assets/Script/Platform/Platform.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public GameObject[] cubes; // 발판을 구성하는 큐브의 갯수
+    private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //플랫폼이 화면 밖으로 나가면 비활성화 
+        if (transform.position.z < -10)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnEnable()
+    {
+        //오브젝트가 다시 활성화 되면
+        Debug.Log("activated");
+
+        stepped = false; //밟힘 상태 리셋
+
+        //큐브 하나당 1/3 확률로 활성화 됨
+        for (int i=0; i<cubes.Length; i++)
+        {
+            if(Random.Range(0,3) == 0)
+            {
+                cubes[i].SetActive(false);
+            }
+            else
+            {
+                cubes[i].SetActive(true);
+            }
+  
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        //플레이어 태그를 가지고 있는 오브젝트와 충돌했고 stepped 가 false 라면
+        if (collision.collider.tag == "Player" && !stepped)
+        {
+            stepped = true;
+            GameManager.Instance.score += 1;
+        }
+    }
+}
