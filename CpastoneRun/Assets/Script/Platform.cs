77ed287dd0ba40c6e59c480ffd3dbfcb8a22d627
dydwo0809 <dydwo0809@naver.com>
2024-01-27 15:52:13 +0900

    Revert "Merge branch 'youjin3'"
    This reverts commit b3e04927af911caa11cabbf7864144c9a9ec8366, reversing
    changes made to 32c87bf8124bb36b6967c5271baa48d3bc5f84da.

--- CpastoneRun/Assets/Script/Platform.cs
+++ CpastoneRun/Assets/Script/Platform.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     public GameObject[] cubes; // 발판을 구성하는 큐브의 갯수
-    public GameObject[] items;//
-
-    //private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+    private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
 
     void Start()
     {
@@ -16,7 +14,11 @@ public class Platform : MonoBehaviour
 
     void Update()
     {
-
+        //플랫폼이 화면 밖으로 나가면 비활성화 
+        if (transform.position.z < -10)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     private void OnEnable()
@@ -24,42 +26,31 @@ public class Platform : MonoBehaviour
         //오브젝트가 다시 활성화 되면
         Debug.Log("activated");
 
-        //stepped = false; //밟힘 상태 리셋
+        stepped = false; //밟힘 상태 리셋
 
-
+        //큐브 하나당 1/3 확률로 활성화 됨
         for (int i=0; i<cubes.Length; i++)
         {
-            //발판 하나당 1/3 확률로 활성화 됨
-            if (Random.Range(0,3) == 0)
+            if(Random.Range(0,3) == 0)
             {
                 cubes[i].SetActive(false);
             }
             else
             {
-                cubes[i].SetActive(true);  
+                cubes[i].SetActive(true);
             }
-
-            //아이템은 1/4 확률로 활성화 됨
-            if (Random.Range(0, 4) == 0)
-            {
-                items[i].SetActive(true);
-            }
-            else
-            {
-                items[i].SetActive(false);
-            }
-
+  
         }
     }
 
-    //플레이어 캐릭터가 발판을 밟았을때 점수를 추가하는 처리 -> 우선 시간과 아이템으로만 점수를 계산하는 것 같아서 주석 처리 해놓
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    //플레이어 태그를 가지고 있는 오브젝트와 충돌했고 stepped 가 false 라면
-    //    if (collision.collider.tag == "Player" && !stepped)
-    //    {
-    //        stepped = true;
-    //        GameManager.Instance.score += 1;
-    //    }
-    //}
+    void OnCollisionEnter(Collision collision)
+    {
+        //플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
+        //플레이어 태그를 가지고 있는 오브젝트와 충돌했고 stepped 가 false 라면
+        if (collision.collider.tag == "Player" && !stepped)
+        {
+            stepped = true;
+            GameManager.Instance.score += 1;
+        }
+    }
 }
--- CpastoneRun/Assets/Script/PlatformSqawner.cs
+++ CpastoneRun/Assets/Script/PlatformSqawner.cs
@@ -9,11 +9,27 @@ public class PlatformSqawner : MonoBehaviour
 
     private int currentPlatformIdx=0;
 
+    private Vector3 poolPosition = new Vector3(-20, 0, -10); //pool의 위치
+
     void Start()
     {
+        //사용할 현재 순번의 발판 게임오브젝트 활성화 -> OnEnable메서드가 실행됨(군데군데 랜덤 생3)
+        platforms[currentPlatformIdx].SetActive(true);
 
+        //현재 순번의 발판을 화면 재배치
+        platforms[currentPlatformIdx].transform.position = new Vector3(0, 0, 6);
+
+        currentPlatformIdx++;
+
+        if (currentPlatformIdx >= platforms.Length)
+        {
+            currentPlatformIdx = 0;
+        }
+
+        transform.position = new Vector3(0, 0, 0);
     }
 
+    // Update is called once per frame
     void Update()
     {
         //게임 오버 상태에서는 동작하지 않음
@@ -22,25 +38,29 @@ public class PlatformSqawner : MonoBehaviour
             return;
         }
 
-        //발판 두개씩 생
-        if (transform.position.z <= -12) 
+        if (transform.position.z <= -6)
         {
+            //이전꺼는 disable
+            if (currentPlatformIdx != 0)
+            {
+                platforms[currentPlatformIdx - 1].SetActive(true);
+                Debug.Log(currentPlatformIdx + ": disabled");
+            }
 
             //사용할 현재 순번의 발판 게임오브젝트 활성화 -> OnEnable메서드가 실행됨(군데군데 랜덤 생성)
-            platforms[currentPlatformIdx].SetActive(false);
             platforms[currentPlatformIdx].SetActive(true);
 
             //현재 순번의 발판을 화면 재배치
-            platforms[currentPlatformIdx].transform.position = new Vector3(0,0,(platforms.Length-1)*6);
-            currentPlatformIdx++;
+            platforms[currentPlatformIdx].transform.position = new Vector3(0,0,12);
 
+            currentPlatformIdx++;
 
             if (currentPlatformIdx >= platforms.Length)
             {
                 currentPlatformIdx = 0;
             }
 
-            transform.position = new Vector3(0, 0, -6);
+            transform.position = new Vector3(0, 0, 0);
         }
 
     }
