3e1b6c6995d7faa04dedf80fad4eb01c75640a36
youjin43 <wlwlfdl3324@naver.com>
2024-01-30 00:29:52 +0900

    아이템 생성

--- CpastoneRun/Assets/Script/Platform/Platform.cs
+++ CpastoneRun/Assets/Script/Platform/Platform.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     public GameObject[] platforms; // 발판을 구성하는 갯수
-    public GameObject[] items; //아이템 배열
-
-    //private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+    public GameObject[] add_items; //아이템 배열
+    public GameObject[] sub_items;
 
     void Start()
     {
-        
     }
 
     void Update()
@@ -23,13 +21,10 @@ public class Platform : MonoBehaviour
         //플랫폼 오브젝트가 다시 활성화 되면
         Debug.Log("Platform is activated");
 
-        //stepped = false; //밟힘 상태 리셋
-
-
-        for (int i=0; i< platforms.Length; i++)
+        for (int i = 0; i < platforms.Length; i++)
         {
             //바닥 하나당 1/3 확률로 비활성화 됨
-            if (Random.Range(0,3) == 0)
+            if (Random.Range(0, 3) == 1)
             {
                 platforms[i].SetActive(false);
             }
@@ -38,27 +33,28 @@ public class Platform : MonoBehaviour
                 platforms[i].SetActive(true);
             }
 
-            ////아이템이 1/5 확률로 활성화됨
-            //if (Random.Range(0, 5) == 0)
-            //{
-            //    items[i].SetActive(true);
-            //}
-            //else
-            //{
-            //    items[i].SetActive(false);
-            //}
+
+            //아이템이 1/5 확률로 활성화됨
+            if (Random.Range(0, 5) == 1)
+            {
+                if (Random.Range(0, 2) == 1)
+                {
+                    add_items[i].SetActive(true);
+                    sub_items[i].SetActive(false);
+                }
+                else
+                {
+                    add_items[i].SetActive(false);
+                    sub_items[i].SetActive(true);
+                }
+            }
+            else
+            {
+                add_items[i].SetActive(false);
+                sub_items[i].SetActive(false);
+            }
 
         }
     }
 
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    //플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
-    //    //플레이어 태그를 가지고 있는 오브젝트와 충돌했고 stepped 가 false 라면
-    //    if (collision.collider.tag == "Player" && !stepped)
-    //    {
-    //        stepped = true;
-    //        GameManager.Instance.score += 1;
-    //    }
-    //}
 }
